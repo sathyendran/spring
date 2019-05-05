3d51c39636456a16d96a4379764f6cb467f736c6
Sathyendran <37661333+sathinarayanan@users.noreply.github.com>
2019-05-05 17:21:31 +0530

    Initial User Data load implemented

--- data-jpa/src/main/java/org/sathya/Application.java
+++ data-jpa/src/main/java/org/sathya/Application.java
@@ -1,13 +1,34 @@
 package org.sathya;
 
+import net.bytebuddy.asm.Advice;
+import org.sathya.user.api.UserRepository;
+import org.sathya.user.bom.User;
+import org.sathya.user.messaging.UserRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class Application {
+import java.util.Arrays;
+import java.util.List;
 
+@SpringBootApplication
+public class Application implements CommandLineRunner {
+
+	@Autowired
+	UserRepository userRepository;
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+
+		List<User> users = Arrays.asList(
+				new User("Sathya", "a", "sathya.a@gmail.com"),
+				new User("Raja", "a", "raja.a@gmail.com"),
+				new User("Ragav", "a", "ragav.a@gmail.com")
+		);
+		userRepository.saveAll(users);
+	}
 }
